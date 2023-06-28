9bce69b308ee54ca580dbe146a33d24f31fe200e
Sri Charan <gundlasricharan@gmail.com>
2023-06-28 17:02:25 +0530

    Initialize Project

---
+++ backend/routes/courseRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { getCourses, setCourse, updateCourse, deleteCourse, getAllCourses, getCoursesById } = require('../controllers/courseController')
+const router = express.Router()
+
+router.get('/',getCourses)
+
+//Getting all the courses
+router.get('/all', getAllCourses)
+
+//Get course by ID 
+router.get('/:id', getCoursesById)
+
+router.post('/addcourse',setCourse)
+
+router.route('/:id').put(updateCourse).delete(deleteCourse)
+
+module.exports = router
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/logo-removebg.png";
+const Header = () => {
+  return (
+    <header class="text-gray-600 body-font">
+      <div class=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Link
+          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          to="/"
+        >
+          <img src={Logo} alt="Logo" style={{ width: "150px" }} />
+        </Link>
+        <nav class="md:m-auto flex flex-wrap items-center text-base justify-center">
+          <Link class="mr-5 hover:text-gray-900" to="/courses">
+            Explore Courses
+          </Link>
+          <Link class="mr-5 hover:text-gray-900">Course Creation</Link>
+          <Link class="mr-5 hover:text-gray-900" to="/about">
+            About
+          </Link>
+          <Link class="mr-5 hover:text-gray-900" to="/contact">
+            Contact Us
+          </Link>
+        </nav>
+        <Link to="register">
+          <button class="inline-flex items-center text-white bg-green-500 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-base mt-4 md:mt-0">
+            Sign Up
+          </button>
+        </Link>
+        <Link to={`login`}>
+          <button class="ml-4 inline-flex items-center bg-gray-100 border-0  py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Login
+          </button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
